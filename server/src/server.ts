c5bb48b2ebaaaf3f1f2ec17fd7b19cb5a4413983
Rohanwagh5 <129928120+Rohanwagh5@users.noreply.github.com>
2025-04-30 22:56:23 +0530

    Update use.cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -20,7 +20,7 @@ app.use(cookieParser());
 
 app.use(express.json())
 
-app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.use(cors({ origin: "https://collaborative-code-editor-byt1.onrender.com", credentials: true }));
 
 connectDB();
 
