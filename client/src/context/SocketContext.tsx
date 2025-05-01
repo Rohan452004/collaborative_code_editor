06587ac93123eba932e8c35b1f6ab10829f446e8
Rohanwagh5 <rohanwagh52005@gmail.com>
2025-05-01 13:12:19 +0530

    local changes

--- client/src/context/SocketContext.tsx
+++ client/src/context/SocketContext.tsx
@@ -28,6 +28,7 @@ export const useSocket = (): SocketContextType => {
 }
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000"
+console.log("Backend URL:", BACKEND_URL);
 
 const SocketProvider = ({ children }: { children: ReactNode }) => {
     const {
--- server/src/server.ts
+++ server/src/server.ts
@@ -46,11 +46,11 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST"],
     credentials: true,
   },
-    	transports: ["websocket", "polling"],
+    transports: ["websocket", "polling"],
 	maxHttpBufferSize: 1e8,
 	pingTimeout: 60000, 
 });
